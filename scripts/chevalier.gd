ec81c31f77286b75d85980856cb7ee4a01d66bf7
Kliraaa <2472632@cmontmorency.qc.ca>
2025-10-17 02:46:50 -0400

    added sounds, did export, added interactivity, platformes

--- scripts/chevalier.gd
+++ scripts/chevalier.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 400.0
+const SPEED = 450.0
 const JUMP_VELOCITY = -550.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,12 +13,42 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		audio_stream_player_2d.stop()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Get the input direction
+	var direction := Input.get_axis("move-left", "move-right")
+	
+	# flip sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	# ANIMATIONS 
+	if direction == 0:
+		animated_sprite.play("idle")
+		audio_stream_player_2d.stop()
+	else:
+		animated_sprite.play("courir")
+		if not audio_stream_player_2d.playing:
+				audio_stream_player_2d.play()
+		
+	#apply movement
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("courir")
+	else: 
+		audio_stream_player_2d.stop()
+		if velocity.y < 0:
+			animated_sprite.play("saut_up")
+		else:
+			animated_sprite.play("tomber-saut_down")
+		
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
--- scripts/sword_2.gd
+++ scripts/sword_2.gd
@@ -1,14 +1,11 @@
 extends Sprite2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var tween = create_tween()
 	tween.set_trans(Tween.TRANS_QUAD)
 	tween.tween_property(self, "position:y", position.y-25, 1).from(position.y+25)
 	tween.tween_property(self, "position:y", position.y+25, 1).from(position.y-25)
 	tween.set_loops()
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
